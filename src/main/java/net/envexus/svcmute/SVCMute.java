efb58789946641b6cdad5736cd4e576e5a7dd8a2
mgmadnesstv <115888232+Envexus@users.noreply.github.com>
2024-07-29 22:45:11 +0100

    Added ACF and other things to the main class

--- src/main/java/net/envexus/svcmute/SVCMute.java
+++ src/main/java/net/envexus/svcmute/SVCMute.java
@@ -1,8 +1,11 @@
 package net.envexus.svcmute;
 
+import co.aikar.commands.BukkitCommandManager;
 import de.maxhenkel.voicechat.api.BukkitVoicechatService;
+import net.envexus.svcmute.commands.SVCMuteCommand;
 import net.envexus.svcmute.configuration.ConfigurationManager;
 import net.envexus.svcmute.integrations.IntegrationManager;
+import net.envexus.svcmute.util.SQLiteHelper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -13,12 +16,20 @@ public final class SVCMute extends JavaPlugin {
     public static final Logger LOGGER = Logger.getLogger(PLUGIN_ID);
 
     private MuteCheckPlugin voicechatPlugin;
+    private SQLiteHelper sqliteHelper;
+    private IntegrationManager integrationManager;
 
     @Override
     public void onEnable() {
         ConfigurationManager messageManager = new ConfigurationManager(this);
-        IntegrationManager integrationManager = new IntegrationManager();
 
+        // Initialize SQLiteHelper
+        sqliteHelper = new SQLiteHelper(this);
+
+        // Initialize IntegrationManager with SQLiteHelper
+        integrationManager = new IntegrationManager(sqliteHelper);
+
+        // Register voice chat plugin
         BukkitVoicechatService service = getServer().getServicesManager().load(BukkitVoicechatService.class);
         if (service != null) {
             voicechatPlugin = new MuteCheckPlugin(integrationManager, messageManager);
@@ -27,6 +38,12 @@ public final class SVCMute extends JavaPlugin {
         } else {
             LOGGER.info("Failed to register voice chat mutecheck plugin");
         }
+
+        // Initialize ACF Command Manager
+        BukkitCommandManager manager = new BukkitCommandManager(this);
+
+        // Register commands
+        manager.registerCommand(new SVCMuteCommand(sqliteHelper, this, integrationManager));
     }
 
     @Override
